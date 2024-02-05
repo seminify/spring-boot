d12f5744f162003d6d7799b495fd8c60d63b1aa0
seminify <seminify@gmail.com>
2024-02-05 18:33:44 +0900

    데이터베이스를 위한 스프링 부트 설정

--- src/test/java/org/seminify/app/AppApplicationTests.java
+++ src/test/java/org/seminify/app/AppApplicationTests.java
@@ -3,13 +3,9 @@ package org.seminify.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import lombok.extern.log4j.Log4j2;
-
 @SpringBootTest
-@Log4j2
 public class AppApplicationTests {
 	@Test
 	public void contextLoads() {
-		log.info("contextLoads");
 	}
 }
