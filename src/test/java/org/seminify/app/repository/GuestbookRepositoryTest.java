86913a1d8c4af35d7408f9a2905a1c5bf451a113
seminify <seminify@gmail.com>
2024-02-06 08:33:56 +0900

    Querydsl 테스트

--- src/test/java/org/seminify/app/repository/GuestbookRepositoryTest.java
+++ src/test/java/org/seminify/app/repository/GuestbookRepositoryTest.java
@@ -4,10 +4,15 @@ import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.seminify.app.entity.Guestbook;
+import org.seminify.app.entity.QGuestbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.core.BooleanBuilder;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -35,5 +40,32 @@ public class GuestbookRepositoryTest {
             guestbook.setTitle("Changed Title");
             guestbook.setContent("Changed Content");
         });
+    }
+
+    @Test
+    public void testQuery1() {
+        var pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+        var guestbook = QGuestbook.guestbook;
+        var keyword = "1";
+        var builder = new BooleanBuilder();
+        var expression = guestbook.title.contains(keyword);
+        builder.and(expression);
+        var result = guestbookRepository.findAll(builder, pageable);
+        result.forEach(log::info);
+    }
+
+    @Test
+    public void testQuery2() {
+        var pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+        var guestbook = QGuestbook.guestbook;
+        var keyword = "1";
+        var builder = new BooleanBuilder();
+        var exTitle = guestbook.title.contains(keyword);
+        var exContent = guestbook.content.contains(keyword);
+        var exAll = exTitle.or(exContent);
+        builder.and(exAll);
+        builder.and(guestbook.gno.gt(0L));
+        var result = guestbookRepository.findAll(builder, pageable);
+        result.forEach(log::info);
     }
 }
