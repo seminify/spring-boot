33d1c0e17c99a03032d4520df7b421fc81bab7e7
seminify <seminify@gmail.com>
2024-02-05 19:45:40 +0900

    정렬 조건 추가하기

--- src/test/java/org/seminify/app/repository/MemoRepositoryTest.java
+++ src/test/java/org/seminify/app/repository/MemoRepositoryTest.java
@@ -7,6 +7,7 @@ import org.seminify.app.entity.Memo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.extern.log4j.Log4j2;
@@ -69,5 +70,18 @@ public class MemoRepositoryTest {
         log.info("Has Next Page? : " + result.hasNext());
         log.info("First Page? : " + result.isFirst());
         result.forEach(log::info);
+    }
+
+    @Test
+    public void testSort() {
+        var sort1 = Sort.by("mno").descending();
+        var sort2 = Sort.by("memoText").ascending();
+        var sortAll = sort1.and(sort2);
+        var pageable1 = PageRequest.of(0, 10, sort1);
+        var pageable2 = PageRequest.of(0, 10, sortAll);
+        var result1 = memoRepository.findAll(pageable1);
+        var result2 = memoRepository.findAll(pageable2);
+        result1.forEach(log::info);
+        result2.forEach(log::info);
     }
 }
