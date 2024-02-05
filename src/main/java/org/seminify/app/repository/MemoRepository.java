a05de622ad489cd06e680bf7097e6bae66bc5e83
seminify <seminify@gmail.com>
2024-02-05 19:31:56 +0900

    JpaRepository 인터페이스

---
+++ src/main/java/org/seminify/app/repository/MemoRepository.java
@@ -0,0 +1,9 @@
+package org.seminify.app.repository;
+
+import org.seminify.app.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+}
