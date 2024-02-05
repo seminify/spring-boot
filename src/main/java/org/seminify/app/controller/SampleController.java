0e57a149c598cc103b9019b286a914628180ba12
seminify <seminify@gmail.com>
2024-02-05 20:32:35 +0900

    inline 속성

--- src/main/java/org/seminify/app/controller/SampleController.java
+++ src/main/java/org/seminify/app/controller/SampleController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -29,5 +30,20 @@ public class SampleController {
             return dto;
         }).toList();
         model.addAttribute("list", list);
+    }
+
+    @GetMapping("ex3")
+    public void ex3() {
+        log.info("ex3");
+    }
+
+    @GetMapping("exInline")
+    public String exInline(RedirectAttributes redirectAttributes) {
+        log.info("exInline");
+        var dto = SampleDTO.builder().sno(100L).first("First 100").last("Last 100").regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+        return "redirect:/sample/ex3";
     }
 }
