b0225138a8c6219ada187446ee9f0477e775238b
seminify <seminify@gmail.com>
2024-02-06 08:24:24 +0900

    엔티티 클래스와 Querydsl 설정

---
+++ src/main/java/org/seminify/app/entity/Guestbook.java
@@ -0,0 +1,30 @@
+package org.seminify.app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@Getter
+@NoArgsConstructor
+@ToString
+public class Guestbook extends BaseEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long gno;
+    @Column(length = 100, nullable = false)
+    private String title;
+    @Column(length = 1500, nullable = false)
+    private String content;
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
