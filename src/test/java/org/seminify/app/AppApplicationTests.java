9d5b75e5597ccc6761538506bfb3e7469a850fc4
seminify <seminify@gmail.com>
2024-02-05 18:27:46 +0900

    테스트 코드의 실행

--- src/test/java/org/seminify/app/AppApplicationTests.java
+++ src/test/java/org/seminify/app/AppApplicationTests.java
@@ -3,9 +3,13 @@ package org.seminify.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootTest
+@Log4j2
 public class AppApplicationTests {
 	@Test
 	public void contextLoads() {
+		log.info("contextLoads");
 	}
 }
