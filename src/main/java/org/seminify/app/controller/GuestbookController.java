e9fbc35b7829028d34f9d414d7758ca0303cb0a2
seminify <seminify@gmail.com>
2024-02-06 09:08:15 +0900

    컨트롤러와 화면에서의 목록 처리

--- src/main/java/org/seminify/app/controller/GuestbookController.java
+++ src/main/java/org/seminify/app/controller/GuestbookController.java
@@ -1,18 +1,32 @@
 package org.seminify.app.controller;
 
+import org.seminify.app.dto.PageRequestDTO;
+import org.seminify.app.service.GuestbookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @RequestMapping("guestbook")
+@RequiredArgsConstructor
 @Log4j2
 public class GuestbookController {
-    @GetMapping({ "/", "list" })
-    public String list() {
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index() {
+        log.info("index");
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
         log.info("list");
-        return "/guestbook/list";
+        log.info(pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
